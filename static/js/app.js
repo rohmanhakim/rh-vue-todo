aed94274abb9d11907f6087462512ac1a4c312f2
rohmanhakim <rohmanhakim@live.com>
2016-05-28 18:35:58 +0700

    Fixed form stills bind to the last added task after added to array when adding new task. Added feature for deleting a task in frontend

--- static/js/app.js
+++ static/js/app.js
@@ -22,6 +22,8 @@ new Vue({
         getAllTasks: function() {
 
             this.$http.get('http://localhost:8080/task/all').success(function(tasks) {
+
+                // set the tasks array equals to the 'tasks' property of the response
                 this.$set('tasksContainer', tasks);
             }).error(function(error) {
                 console.log(error);
@@ -32,21 +34,36 @@ new Vue({
         // adds an event to the existing events array
         addTask: function(){
             if(this.task.title) {
-                /*                this.taskContainer.tasks.push(this.task);
-                this.task = { title: '', notes: ''};*/
                 this.$http.post('http://localhost:8080/task', this.task).success(function(response) {
-                  this.tasksContainer.tasks.push(this.task);
-                  console.log("Task added!");
+
+                    // set the task id equals to the 'id' property of the response
+                    this.task.id = response.id
+
+                    // push task to the array
+                    this.tasksContainer.tasks.push(this.task);
+
+                    // set the task to empty
+                    this.task = { id: '', title: '', notes: '' };
+
+                    console.log("Task added!");
                 }).error(function(error) {
-                  console.log(error);
+                    console.log(error);
                 });
             }
         },
 
         deleteTask: function (index) {
             if(confirm("Are you sure you want to delete this task?")){
-                // $remove is a Vue convenience method similar to splice
-                this.tasksContainer.tasks.$remove(this.tasks[index]);
+
+                this.$http.delete('http://localhost:8080/task/' + this.tasksContainer.tasks[index].id)
+                    .success(function(response) {
+
+                        // $remove is a Vue convenience method similar to splice
+                        this.tasksContainer.tasks.$remove(this.tasksContainer.tasks[index]);
+                    })
+                    .error(function(error) {
+                        console.log(error);
+                    });
             }
         }
     }
