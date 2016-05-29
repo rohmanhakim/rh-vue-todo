fc0a0f2cc6d89e38a1dd18c18bce3c0ff72a3c44
rohmanhakim <rohmanhakim@live.com>
2016-05-29 14:21:10 +0700

    Changed the popup for delete task to Vue.js's modal dialog

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,28 @@
+Vue.component('modal', {
+    template: '#modal-template',
+    props: ['show', 'index','title', 'message','type','callback'],
+    events: {
+        onShowConfirmationModal: function(index,title,message,type,callback) {
+            this.show = true;           // specify if this modal should be shown or not
+            this.index = index;         // specify the related task's index
+            this.title = title;         // specify this modal dialog's title
+            this.message = message;     // specify the this modal dialog's message
+            this.type = type;           // specify if this modal dialog type is positive or negative
+            this.callback = callback;   // specify the callback for this modal dialog
+        }
+    },
+    methods: {
+        onCancel: function () {
+            this.show = false;
+        },
+        onConfirm: function(index) {
+            this.$dispatch(this.callback,index);
+            console.log("Sending dispatch on confirm delete");
+            this.show = false;
+        }
+    }
+});
+
 new Vue({
     // we want to target the div with an id of 'events'
     el: '#tasks',
@@ -13,6 +38,14 @@ new Vue({
     ready: function() {
         // when the application loads, we want to call the method that initializes some data
         this.getAllTasks();
+    },
+    
+    //anything within the events options will listen for the specified events that dispatched fro the child component/instance
+    events: {
+        //this will listen for 'onConfirmDeleteTask' from child who sends it
+        'onConfirmDeleteTask' : function(index) {
+            this.deleteTask(index);
+        }
     },
 
     //methods we want to use in our application are registered here
@@ -52,19 +85,23 @@ new Vue({
             }
         },
 
+        //delete a task
         deleteTask: function (index) {
-            if(confirm("Are you sure you want to delete this task?")){
-
+                console.log("receive dispatch event on confirm delete");
                 this.$http.delete('http://localhost:8080/task/' + this.tasksContainer.tasks[index].id)
-                    .success(function(response) {
-
-                        // $remove is a Vue convenience method similar to splice
-                        this.tasksContainer.tasks.$remove(this.tasksContainer.tasks[index]);
-                    })
-                    .error(function(error) {
+                .success( function(response) {
+                // $remove is a Vue convenience method similar to splice
+                    this.tasksContainer.tasks.$remove(this.tasksContainer.tasks[index]);
+                })
+                .error( function(error) {
                         console.log(error);
-                    });
-            }
+                });
+        },
+
+        showDeleteTaskModal: function (index) {
+            //broadcast will send the specified event to all child of this vue's instance
+            //here, we pass fouur argument to the receiving child: the index of the task, the modal dialog's message, the type of the modal dialog ("success" for positive, and "danger" for negative), and its callback wich will be listened by this instance's "event" options
+            this.$broadcast('onShowConfirmationModal',index,"Confirm delete","Are you sure want to delete this task?","success",'onConfirmDeleteTask');
         }
     }
 });
