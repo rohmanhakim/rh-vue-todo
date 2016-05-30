82cdfe4280294e825e56c8604a4f227881c5a565
rohmanhakim <rohmanhakim@live.com>
2016-05-30 16:13:40 +0700

    Added feature to update task in frontend. Making modal component reusable by extended it to "confirmation modal" and "edit task modal"

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,8 @@
+Vue.component('modal', {
+    template: '#modal-template',
+    props: ['show']
+});
+
 Vue.component('confirmation-modal', {
     template: '#confirmation-modal-template',
     props: ['show', 'index','title', 'message','type','callback'],
@@ -18,6 +23,36 @@ Vue.component('confirmation-modal', {
         onConfirm: function(index) {
             this.$dispatch(this.callback,index);
             console.log("Sending dispatch on confirm delete");
+            this.show = false;
+        }
+    }
+});
+
+Vue.component('edit-task-modal', {
+    template: '#edit-task-modal-template',
+    props: ['show', 'index','title','type','callback'],
+    data: function() {
+        return {
+            task: { id: '', title: '', notes: ''}
+        };
+    },
+    events: {
+        onShowModal: function(index,title,task,type,callback) {
+            this.show = true;           // specify if this modal should be shown or not
+            this.index = index;         // specify the related task's index
+            this.title = title;         // specify this modal dialog's title
+            this.task = task;
+            this.type = type;           // specify if this modal dialog type is positive or negative
+            this.callback = callback;   // specify the callback for this modal dialog
+        }
+    },
+    methods: {
+        onCancel: function () {
+            this.show = false;
+        },
+        onConfirm: function(index) {
+            this.$dispatch(this.callback,index,this.task);
+            console.log("Sending dispatch on confirm update");
             this.show = false;
         }
     }
@@ -45,6 +80,10 @@ new Vue({
         //this will listen for 'onConfirmDeleteTask' from child who sends it
         'onConfirmDeleteTask' : function(index) {
             this.deleteTask(index);
+        },
+        
+        'onConfirmUpdateTask' : function(index,task) {
+            this.updateTask(index,task)
         }
     },
 
@@ -97,11 +136,26 @@ new Vue({
                         console.log(error);
                 });
         },
+        
+        //update a task
+        updateTask: function (index,task) {
+            console.log("receive dispatch event on confirm update");
+            this.$http.put('http://localhost:8080/task/' + this.tasksContainer.tasks[index].id, task )
+            .success( function(response) {
+                this.tasksContainer.tasks[index] = task;
+            })
+            .error( function(error) {
+               console.log(error); 
+            });
+        },
 
         showDeleteTaskModal: function (index) {
             //broadcast will send the specified event to all child of this vue's instance
             //here, we pass fouur argument to the receiving child: the index of the task, the modal dialog's message, the type of the modal dialog ("success" for positive, and "danger" for negative), and its callback wich will be listened by this instance's "event" options
             this.$broadcast('onShowConfirmationModal',index,"Confirm delete","Are you sure want to delete this task?","danger",'onConfirmDeleteTask');
+        },
+        showEditTaskModal: function(index) {
+            this.$broadcast('onShowModal',index,"Edit task",this.tasksContainer.tasks[index],"success",'onConfirmUpdateTask');
         }
     }
 });
